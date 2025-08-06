c4ae65e843f88d2ae37266c4a79cf7efcd435a79
Simone Basso <bassosimone@gmail.com>
2025-08-06 18:15:04 +0200

    feat(uart): port to ARM64
    This is funny and not obvious because of the non-coherent
    memory model exported by devices, so we need to use memory
    barriers to ensure sequential completion ordering.

--- kernel/asm/arm64.h
+++ kernel/asm/arm64.h
@@ -8,6 +8,32 @@
 
 #include <kernel/sys/types.h>
 
+// WFI: wait for interrupts
 static inline void wfi(void) { __asm__ volatile("wfi"); }
+
+// DSB: data synchronization barrier.
+//
+// Similar to Zig's `.SeqCst`.
+//
+// Use this for:
+//
+// 1. MMIO device register writes
+//
+// 2. Page table updates
+//
+// 3. System state transitions
+static inline void dsb_sy(void) { __asm__ volatile("dsb sy" ::: "memory"); }
+
+// ISB: instruction synchronization barrier.
+//
+// Required after system register writes (e.g., TTBR).
+static inline void isb(void) { __asm__ volatile("isb" ::: "memory"); }
+
+// DMB: data memory barrier.
+//
+// Before a read, similar to Zig's `.Acquire`.
+//
+// After a write, similar to Zig's `.Release`.
+static inline void dmb_sy(void) { __asm__ volatile("dmb sy" ::: "memory"); }
 
 #endif // KERNEL_ASM_ARM64
---
+++ kernel/tty/uart_arm64.c
@@ -0,0 +1,77 @@
+// File: kernel/tty/uart_arm64.c
+// Purpose: Universal-asynchronous receiver-transmitter (UART) driver for ARM64.
+// SPDX-License-Identifier: MIT
+// Adapted from: https://github.com/nuta/operating-system-in-1000-lines
+
+#include <kernel/asm/arm64.h>
+#include <kernel/errno.h>
+#include <kernel/tty/uart.h>
+
+// PL011 UART base address in QEMU virt
+#define UART0_BASE 0x09000000
+
+// Data register
+#define UARTDR (*(volatile uint32_t *)(UART0_BASE + 0x00))
+
+// Flags register
+#define UARTFR (*(volatile uint32_t *)(UART0_BASE + 0x18))
+
+// Control register: enable UART, TX/RX
+#define UARTCR 0x30
+
+// Interrupt mask set/clear register
+#define UARTIMSC 0x38
+
+// Interrupt clear (i.e., acknowledge) register
+#define UARTICR 0x44
+
+// Transmit FIFO full
+#define UARTFR_TXFF (1 << 5)
+
+// Receive FIFO empty
+#define UARTFR_RXFE (1 << 4)
+
+void uart_init(void) {
+	// QEMU's PL011 is pre-initialized.
+	//
+	// However, let's do the exercise of resetting it.
+	volatile uint32_t *cr = (volatile uint32_t *)(UART0_BASE + UARTCR);
+	volatile uint32_t *imsc = (volatile uint32_t *)(UART0_BASE + UARTIMSC);
+	volatile uint32_t *icr = (volatile uint32_t *)(UART0_BASE + UARTICR);
+
+	// Disable UART and "push" changes
+	*cr = 0;
+	dsb_sy();
+
+	// Mask all IRQs
+	*imsc = 0x0;
+	// Clear any pending IRQs
+	*icr = 0x7FF;
+	// "Push" changes
+	dsb_sy();
+
+	// Enable TXE | RXE | UARTEN and "push" changes
+	*cr = (1 << 9) | (1 << 8) | (1 << 0);
+	dsb_sy();
+}
+
+int16_t uart_try_read(void) {
+	if (!uart_poll_read()) {
+		return -EAGAIN;
+	}
+	dsb_sy(); // Ensure the subsequent read "happens after"
+	return (int16_t)(UARTDR & 0xFF);
+}
+
+bool uart_poll_read(void) { return (UARTFR & UARTFR_RXFE) == 0; }
+
+int16_t uart_try_write(uint8_t ch) {
+	if (!uart_poll_write()) {
+		return -EAGAIN;
+	}
+	dsb_sy(); // Ensure the subsequent write "happens after"
+	UARTDR = ch;
+	return 0;
+}
+
+bool uart_poll_write(void) { return (UARTFR & UARTFR_TXFF) == 0; }
