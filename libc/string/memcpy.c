a5403758534d076a79542cffc0319184e553299f
Simone Basso <bassosimone@gmail.com>
2025-08-09 00:46:12 +0200

    feat(libc): implement memcpy

---
+++ libc/string/memcpy.c
@@ -0,0 +1,15 @@
+// File: libc/string/memcpy.c
+// Purpose: memcpy implementation from musl
+// SPDX-License-Identifier: MIT
+// Adapted from: https://git.musl-libc.org/cgit/musl/tree/src/string/memcpy.c
+
+#include <libc/string/string.h>
+
+void *memcpy(void *dest, const void *src, size_t n) {
+	unsigned char *d = dest;
+	const unsigned char *s = src;
+	for (; n; n--) {
+		*d++ = *s++;
+	}
+	return dest;
+}
--- libc/string/string.h
+++ libc/string/string.h
@@ -6,6 +6,8 @@
 
 #include <kernel/sys/types.h>
 
+void *memcpy(void *dest, const void *src, size_t n);
+
 void *memset(void *dest, int c, size_t n);
 
 int strncmp(const char *_l, const char *_r, size_t n);
