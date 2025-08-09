5a393e91f6b61292b2919b833ef910fd6827c06b
Simone Basso <bassosimone@gmail.com>
2025-08-09 04:48:21 +0200

    fix: more precise and aligned layout
    Know exactly where each section starts and end. Also, make sure we are
    always 4 KiB aligned, which simplifies writing VM code.

--- kernel/boot/layout.h
+++ kernel/boot/layout.h
@@ -8,11 +8,30 @@
 // Start of kernel code
 extern char __kernel_base[];
 
+// End of kernel code
+extern char __kernel_end[];
+
+// Start of rodata
+extern char __rodata_base[];
+
+// End of rodata
+extern char __rodata_end[];
+
+// Start of data
+extern char __data_base[];
+
+// End of data
+extern char __data_end[];
+
 // Start of BSS section
 extern char __bss[];
+#define __bss_base __bss
 
 // End of BSS section
 extern char __bss_end[];
+
+// Bottom of kernel stack
+extern char __stack_bottom[];
 
 // Top of kernel stack
 extern char __stack_top[];
